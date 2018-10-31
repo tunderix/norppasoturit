ab8afcb0b1d187e1fa6d30c81b5b3dd3318082e1
Sami Repo <sami.repo@valamis.com>
2018-10-31 18:21:27 +0200

    path by resolve

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static( __dirname + './dist/norppasoturit/'));
+app.use(express.static( path.resolve('dist', 'norppasoturit')));
 
 app.get('/*', function(req,res) {
     
-res.sendFile( __dirname + '/dist/norppasoturit/index.html' );
+res.sendFile( path.resolve('dist', 'norppasoturit', 'index.html') );
 });
 
 // Start the app by listening on the default Heroku port
